af5d4ec885e63495f4bd951652089a1e388f75b4
noahsilencelab <noah@silencelaboratories.com>
2024-06-10 13:31:30 +0700

    Implement test cases for typed signing, move mock object to separate file

--- src/__test__/index.test.ts
+++ src/__test__/index.test.ts
@@ -6,12 +6,23 @@ import { SnapError, SnapErrorCode } from '../error';
 import { DAPP_URL_STAGING, InternalMethod } from '../permissions';
 import * as simulator from './simulator';
 import { SimpleKeyring } from '../snap/keyring';
-import { DistributedKey, PairingData, SignMetadata, Wallet } from '../types';
-import { IP1KeyShare } from '@silencelaboratories/ecdsa-tss';
-import { delay, fromHexStringToBytes } from '../snap/utils';
-import * as SignAction from '../snap/actions/sign';
+import { DistributedKey, PairingData } from '../types';
+import { delay } from '../snap/utils';
+
 import { TransactionFactory } from '@ethereumjs/tx';
 import { Common, Hardfork } from '@ethereumjs/common';
+import { SignTypedDataVersion } from '@metamask/eth-sig-util';
+import {
+	WALLER_ADDRESS,
+	mockLegacyTx,
+	MOCK_WALLET_ACCOUNT,
+	genMockRunTssSign,
+	mockPersonalMsg,
+	mockSignTypedDataV4,
+	mockEip1559Tx,
+	mockSignTypedDataV3,
+	mockSignTypedDataV1,
+} from './mocks';
 
 const ORIGIN = DAPP_URL_STAGING;
 const INIT_PAIR_PANEL_HEADING = `Hey there! 👋🏻 Welcome to Silent Shard Snap – your gateway to distributed-self custody!`;
@@ -20,8 +31,6 @@ const INIT_PAIR_PANEL_DESCRIPTION = [
 	`👉🏻 Just search for 'Silent Shard' and follow the simple steps to set up your MPC account.`,
 	`Happy to have you onboard! 🥳`,
 ];
-const WALLER_ADDRESS = '0x660265edc169bab511a40c0e049cc1e33774443d';
-const TO_ADDRESS = '0x0c54fccd2e384b4bb6f2e405bf5cbc15a017aafb';
 
 export const DEVICE_NAME =
 	'e2e-test-device' + Math.floor(Math.random() * 1000000);
@@ -30,48 +39,6 @@ interface QrCode {
 	webEncPublicKey: string;
 	signPublicKey: string;
 }
-
-const genMockRunTssSign =
-	(pairingData: any) =>
-	async (
-		hashAlg: string,
-		message: string,
-		messageHashHex: string,
-		signMetadata: SignMetadata,
-		accountId: number,
-		keyShare: IP1KeyShare,
-	) => {
-		if (messageHashHex.startsWith('0x')) {
-			messageHashHex = messageHashHex.slice(2);
-		}
-		if (message.startsWith('0x')) {
-			message = message.slice(2);
-		}
-
-		// TODO: Do we want to simulate token expiration?
-		// let silentShareStorage = await getSilentShareStorage();
-		// let pairingData = silentShareStorage.pairingData;
-		// if (pairingData.tokenExpiration < Date.now() - TOKEN_LIFE_TIME) {
-		// 	pairingData = await refreshPairing();
-		// }
-		let messageHash = fromHexStringToBytes(messageHashHex);
-		if (messageHash.length !== 32) {
-			throw new SnapError(
-				'Invalid length of messageHash, should be 32 bytes',
-				SnapErrorCode.InvalidMessageHashLength,
-			);
-		}
-
-		return await SignAction.sign(
-			pairingData,
-			keyShare,
-			hashAlg,
-			message,
-			messageHash,
-			signMetadata,
-			accountId,
-		);
-	};
 
 describe('test rpc requests to Snap', () => {
 	describe('wrong permission and rejection', () => {
@@ -225,20 +192,7 @@ describe('test rpc requests to Snap', () => {
 			const keyring = new SimpleKeyring({
 				wallets: {
 					[accountId]: {
-						account: {
-							id: 'f4211653-1b5f-4497-b5e6-f7b56824ba21',
-							options: {},
-							address: WALLER_ADDRESS,
-							methods: [
-								'eth_sign',
-								'eth_signTransaction',
-								'eth_signTypedData_v1',
-								'eth_signTypedData_v3',
-								'eth_signTypedData_v4',
-								'personal_sign',
-							],
-							type: 'eip155:eoa',
-						},
+						...MOCK_WALLET_ACCOUNT,
 						distributedKey: keyshareResult.distributedKey,
 					},
 				},
@@ -248,19 +202,6 @@ describe('test rpc requests to Snap', () => {
 			const pairingData = keyshareResult.pairingData;
 			const runTssSign = genMockRunTssSign(pairingData);
 			// Eip1559 sign
-			const mockEip1559Tx: any = {
-				type: '0x2',
-				nonce: '0x1',
-				to: TO_ADDRESS,
-				from: WALLER_ADDRESS,
-				value: '0x0',
-				data: '0x',
-				gasLimit: '0x5208',
-				maxPriorityFeePerGas: '0x3b9aca00',
-				maxFeePerGas: '0x2540be400',
-				accessList: [],
-				chainId: '0xaa36a7',
-			};
 
 			let eip1559SignResult: any = null;
 			keyring
@@ -271,7 +212,7 @@ describe('test rpc requests to Snap', () => {
 				.catch((err) => {
 					console.log('err', err);
 				});
-			await simulator.sign();
+			simulator.sign();
 
 			while (!eip1559SignResult) {
 				await delay(0);
@@ -380,20 +321,7 @@ describe('test rpc requests to Snap', () => {
 			const keyring = new SimpleKeyring({
 				wallets: {
 					[accountId]: {
-						account: {
-							id: 'f4211653-1b5f-4497-b5e6-f7b56824ba21',
-							options: {},
-							address: WALLER_ADDRESS,
-							methods: [
-								'eth_sign',
-								'eth_signTransaction',
-								'eth_signTypedData_v1',
-								'eth_signTypedData_v3',
-								'eth_signTypedData_v4',
-								'personal_sign',
-							],
-							type: 'eip155:eoa',
-						},
+						...MOCK_WALLET_ACCOUNT,
 						distributedKey: keyshareResult.distributedKey,
 					},
 				},
@@ -404,17 +332,6 @@ describe('test rpc requests to Snap', () => {
 			const runTssSign = genMockRunTssSign(pairingData);
 
 			// Legacy sign
-			const mockLegacyTx: any = {
-				type: '0x0',
-				nonce: '0x0',
-				to: TO_ADDRESS,
-				from: WALLER_ADDRESS,
-				value: '0x0',
-				data: '0x',
-				gasLimit: '0x5208',
-				gasPrice: '0x2540be400',
-				chainId: '0xaa36a7',
-			};
 
 			let legacySignResult: any = null;
 			keyring
@@ -425,7 +342,7 @@ describe('test rpc requests to Snap', () => {
 				.catch((err) => {
 					console.log('err', err);
 				});
-			await simulator.sign();
+			simulator.sign();
 
 			while (!legacySignResult) {
 				await delay(0);
@@ -534,20 +451,7 @@ describe('test rpc requests to Snap', () => {
 			const keyring = new SimpleKeyring({
 				wallets: {
 					[accountId]: {
-						account: {
-							id: 'f4211653-1b5f-4497-b5e6-f7b56824ba21',
-							options: {},
-							address: WALLER_ADDRESS,
-							methods: [
-								'eth_sign',
-								'eth_signTransaction',
-								'eth_signTypedData_v1',
-								'eth_signTypedData_v3',
-								'eth_signTypedData_v4',
-								'personal_sign',
-							],
-							type: 'eip155:eoa',
-						},
+						...MOCK_WALLET_ACCOUNT,
 						distributedKey: keyshareResult.distributedKey,
 					},
 				},
@@ -559,15 +463,11 @@ describe('test rpc requests to Snap', () => {
 
 			// Personal sign
 			let personalSignResult: any = null;
-			const exampleMessage = 'Example `personal_sign` message.';
-			const msg = `0x${Buffer.from(exampleMessage, 'utf8').toString(
-				'hex',
-			)}`;
-			console.log('msg', msg);
+
 			keyring
 				.signPersonalMessage(
 					WALLER_ADDRESS,
-					'0x4578616d706c652060706572736f6e616c5f7369676e60206d657373616765',
+					mockPersonalMsg,
 					runTssSign,
 				)
 				.then((resp: any) => {
@@ -576,10 +476,357 @@ describe('test rpc requests to Snap', () => {
 				.catch((err) => {
 					console.log('err', err);
 				});
-			await simulator.sign();
+			simulator.sign();
 			while (!personalSignResult) {
 				await delay(0);
 				console.log('waiting for personalSignResult');
+			}
+
+			await simulator.cleanUpSimulation();
+		});
+
+		it('tss_initPairing, tss_runPairing, tss_runKeygen, V4 typed signing should be success', async () => {
+			const { request } = await installSnap();
+			// Test init pairing
+			const initPairingReq = request({
+				method: InternalMethod.TssInitPairing,
+				origin: ORIGIN,
+				params: [{ isRePair: false }],
+			});
+
+			const ui = await initPairingReq.getInterface();
+			expect(ui.type).toBe(DialogType.Confirmation);
+			const prompt = INIT_PAIR_PANEL_HEADING;
+			const description = INIT_PAIR_PANEL_DESCRIPTION;
+			expect(ui).toRender(
+				panel([
+					heading(prompt),
+					divider(),
+					...description.map((t) => text(t)),
+				]),
+			);
+
+			await ui.ok();
+
+			const initPairingJson: any = (await initPairingReq).response;
+			const initPairingResult =
+				initPairingJson.result as InitPairingResponse;
+			const qrCode = initPairingResult.qrCode;
+			const qrCodeObj = JSON.parse(qrCode) as QrCode;
+
+			expect(qrCodeObj.pairingId).toEqual(expect.any(String));
+			expect(qrCodeObj.webEncPublicKey).toEqual(expect.any(String));
+			expect(qrCodeObj.signPublicKey).toEqual(expect.any(String));
+
+			// Test run pairing
+			await simulator.signIn();
+			await simulator.pairing(qrCode);
+
+			const runPairingReq = request({
+				method: InternalMethod.TssRunPairing,
+				origin: ORIGIN,
+			});
+
+			const runPairingJson: any = (await runPairingReq).response;
+			const runPairingResult =
+				runPairingJson.result as RunPairingResponse;
+			expect(runPairingResult.deviceName).toEqual(DEVICE_NAME);
+			expect(runPairingResult.address).toBeNull();
+
+			// Test key generation
+			const keygenReq = request({
+				method: InternalMethod.TssRunKeygen,
+				origin: ORIGIN,
+			});
+			await simulator.keygen();
+
+			const keyGenJson: any = (await keygenReq).response;
+			const runKeyGenResult = keyGenJson.result as RunKeygenResponse;
+			expect(runKeyGenResult.address).toEqual(expect.any(String));
+
+			request({
+				method: InternalMethod.TssRunBackup,
+				origin: ORIGIN,
+			});
+
+			// Test signing
+			const keyshareReq = request({
+				method: InternalMethod.E2eTestGetKeyShare,
+				origin: ORIGIN,
+			});
+			const keyshareReqJson: any = (await keyshareReq).response;
+			const keyshareResult = keyshareReqJson.result as {
+				distributedKey: DistributedKey;
+				pairingData: PairingData;
+			};
+
+			const accountId = keyshareResult.distributedKey.accountId;
+			const keyring = new SimpleKeyring({
+				wallets: {
+					[accountId]: {
+						...MOCK_WALLET_ACCOUNT,
+						distributedKey: keyshareResult.distributedKey,
+					},
+				},
+				requests: {},
+			});
+
+			const pairingData = keyshareResult.pairingData;
+			const runTssSign = genMockRunTssSign(pairingData);
+
+			// Typed sign
+			let typedV4SignResult: any = null;
+
+			keyring
+				.signTypedData(
+					WALLER_ADDRESS,
+					mockSignTypedDataV4,
+					{ version: SignTypedDataVersion.V4 },
+					'eth_signTypedData_v4',
+					runTssSign,
+				)
+				.then((resp: any) => {
+					typedV4SignResult = resp;
+				})
+				.catch((err) => {
+					console.log('err', err);
+				});
+			simulator.sign();
+			while (!typedV4SignResult) {
+				await delay(0);
+				console.log('waiting for typedV4SignResult');
+			}
+
+			
+
+			await simulator.cleanUpSimulation();
+		});
+
+		it('tss_initPairing, tss_runPairing, tss_runKeygen, V3 typed signing should be success', async () => {
+			const { request } = await installSnap();
+			// Test init pairing
+			const initPairingReq = request({
+				method: InternalMethod.TssInitPairing,
+				origin: ORIGIN,
+				params: [{ isRePair: false }],
+			});
+
+			const ui = await initPairingReq.getInterface();
+			expect(ui.type).toBe(DialogType.Confirmation);
+			const prompt = INIT_PAIR_PANEL_HEADING;
+			const description = INIT_PAIR_PANEL_DESCRIPTION;
+			expect(ui).toRender(
+				panel([
+					heading(prompt),
+					divider(),
+					...description.map((t) => text(t)),
+				]),
+			);
+
+			await ui.ok();
+
+			const initPairingJson: any = (await initPairingReq).response;
+			const initPairingResult =
+				initPairingJson.result as InitPairingResponse;
+			const qrCode = initPairingResult.qrCode;
+			const qrCodeObj = JSON.parse(qrCode) as QrCode;
+
+			expect(qrCodeObj.pairingId).toEqual(expect.any(String));
+			expect(qrCodeObj.webEncPublicKey).toEqual(expect.any(String));
+			expect(qrCodeObj.signPublicKey).toEqual(expect.any(String));
+
+			// Test run pairing
+			await simulator.signIn();
+			await simulator.pairing(qrCode);
+
+			const runPairingReq = request({
+				method: InternalMethod.TssRunPairing,
+				origin: ORIGIN,
+			});
+
+			const runPairingJson: any = (await runPairingReq).response;
+			const runPairingResult =
+				runPairingJson.result as RunPairingResponse;
+			expect(runPairingResult.deviceName).toEqual(DEVICE_NAME);
+			expect(runPairingResult.address).toBeNull();
+
+			// Test key generation
+			const keygenReq = request({
+				method: InternalMethod.TssRunKeygen,
+				origin: ORIGIN,
+			});
+			await simulator.keygen();
+
+			const keyGenJson: any = (await keygenReq).response;
+			const runKeyGenResult = keyGenJson.result as RunKeygenResponse;
+			expect(runKeyGenResult.address).toEqual(expect.any(String));
+
+			request({
+				method: InternalMethod.TssRunBackup,
+				origin: ORIGIN,
+			});
+
+			// Test signing
+			const keyshareReq = request({
+				method: InternalMethod.E2eTestGetKeyShare,
+				origin: ORIGIN,
+			});
+			const keyshareReqJson: any = (await keyshareReq).response;
+			const keyshareResult = keyshareReqJson.result as {
+				distributedKey: DistributedKey;
+				pairingData: PairingData;
+			};
+
+			const accountId = keyshareResult.distributedKey.accountId;
+			const keyring = new SimpleKeyring({
+				wallets: {
+					[accountId]: {
+						...MOCK_WALLET_ACCOUNT,
+						distributedKey: keyshareResult.distributedKey,
+					},
+				},
+				requests: {},
+			});
+
+			const pairingData = keyshareResult.pairingData;
+			const runTssSign = genMockRunTssSign(pairingData);
+
+			// Typed sign
+			let typedV3SignResult: any = null;
+
+			keyring
+				.signTypedData(
+					WALLER_ADDRESS,
+					mockSignTypedDataV3,
+					{ version: SignTypedDataVersion.V3 },
+					'eth_signTypedData_v3',
+					runTssSign,
+				)
+				.then((resp: any) => {
+					typedV3SignResult = resp;
+				})
+				.catch((err) => {
+					console.log('err', err);
+				});
+			simulator.sign();
+			while (!typedV3SignResult) {
+				await delay(0);
+				console.log('waiting for typedV3SignResult');
+			}
+
+			await simulator.cleanUpSimulation();
+		});
+
+		it('tss_initPairing, tss_runPairing, tss_runKeygen, V1 typed signing should be success', async () => {
+			const { request } = await installSnap();
+			// Test init pairing
+			const initPairingReq = request({
+				method: InternalMethod.TssInitPairing,
+				origin: ORIGIN,
+				params: [{ isRePair: false }],
+			});
+
+			const ui = await initPairingReq.getInterface();
+			expect(ui.type).toBe(DialogType.Confirmation);
+			const prompt = INIT_PAIR_PANEL_HEADING;
+			const description = INIT_PAIR_PANEL_DESCRIPTION;
+			expect(ui).toRender(
+				panel([
+					heading(prompt),
+					divider(),
+					...description.map((t) => text(t)),
+				]),
+			);
+
+			await ui.ok();
+
+			const initPairingJson: any = (await initPairingReq).response;
+			const initPairingResult =
+				initPairingJson.result as InitPairingResponse;
+			const qrCode = initPairingResult.qrCode;
+			const qrCodeObj = JSON.parse(qrCode) as QrCode;
+
+			expect(qrCodeObj.pairingId).toEqual(expect.any(String));
+			expect(qrCodeObj.webEncPublicKey).toEqual(expect.any(String));
+			expect(qrCodeObj.signPublicKey).toEqual(expect.any(String));
+
+			// Test run pairing
+			await simulator.signIn();
+			await simulator.pairing(qrCode);
+
+			const runPairingReq = request({
+				method: InternalMethod.TssRunPairing,
+				origin: ORIGIN,
+			});
+
+			const runPairingJson: any = (await runPairingReq).response;
+			const runPairingResult =
+				runPairingJson.result as RunPairingResponse;
+			expect(runPairingResult.deviceName).toEqual(DEVICE_NAME);
+			expect(runPairingResult.address).toBeNull();
+
+			// Test key generation
+			const keygenReq = request({
+				method: InternalMethod.TssRunKeygen,
+				origin: ORIGIN,
+			});
+			await simulator.keygen();
+
+			const keyGenJson: any = (await keygenReq).response;
+			const runKeyGenResult = keyGenJson.result as RunKeygenResponse;
+			expect(runKeyGenResult.address).toEqual(expect.any(String));
+
+			request({
+				method: InternalMethod.TssRunBackup,
+				origin: ORIGIN,
+			});
+
+			// Test signing
+			const keyshareReq = request({
+				method: InternalMethod.E2eTestGetKeyShare,
+				origin: ORIGIN,
+			});
+			const keyshareReqJson: any = (await keyshareReq).response;
+			const keyshareResult = keyshareReqJson.result as {
+				distributedKey: DistributedKey;
+				pairingData: PairingData;
+			};
+
+			const accountId = keyshareResult.distributedKey.accountId;
+			const keyring = new SimpleKeyring({
+				wallets: {
+					[accountId]: {
+						...MOCK_WALLET_ACCOUNT,
+						distributedKey: keyshareResult.distributedKey,
+					},
+				},
+				requests: {},
+			});
+
+			const pairingData = keyshareResult.pairingData;
+			const runTssSign = genMockRunTssSign(pairingData);
+
+			// Typed sign
+			let typedV1SignResult: any = null;
+
+			keyring
+				.signTypedData(
+					WALLER_ADDRESS,
+					mockSignTypedDataV1,
+					{ version: SignTypedDataVersion.V1 },
+					'eth_signTypedData_v1',
+					runTssSign,
+				)
+				.then((resp: any) => {
+					typedV1SignResult = resp;
+				})
+				.catch((err) => {
+					console.log('err', err);
+				});
+			simulator.sign();
+			while (!typedV1SignResult) {
+				await delay(0);
+				console.log('waiting for typedV1SignResult');
 			}
 
 			await simulator.cleanUpSimulation();
---
+++ src/__test__/mocks.ts
@@ -0,0 +1,265 @@
+import { IP1KeyShare } from '@silencelaboratories/ecdsa-tss';
+import { SignMetadata } from '../types';
+import { fromHexStringToBytes } from '../snap/utils';
+import { SnapError, SnapErrorCode } from '../error';
+import * as SignAction from '../snap/actions/sign';
+
+export const WALLER_ADDRESS = '0x660265edc169bab511a40c0e049cc1e33774443d';
+export const TO_ADDRESS = '0x0c54fccd2e384b4bb6f2e405bf5cbc15a017aafb';
+
+export const MOCK_WALLET_ACCOUNT: any = {
+	account: {
+		id: 'f4211653-1b5f-4497-b5e6-f7b56824ba21',
+		options: {},
+		address: WALLER_ADDRESS,
+		methods: [
+			'eth_sign',
+			'eth_signTransaction',
+			'eth_signTypedData_v1',
+			'eth_signTypedData_v3',
+			'eth_signTypedData_v4',
+			'personal_sign',
+		],
+		type: 'eip155:eoa',
+	},
+};
+
+export const genMockRunTssSign =
+	(pairingData: any) =>
+	async (
+		hashAlg: string,
+		message: string,
+		messageHashHex: string,
+		signMetadata: SignMetadata,
+		accountId: number,
+		keyShare: IP1KeyShare,
+	) => {
+		if (messageHashHex.startsWith('0x')) {
+			messageHashHex = messageHashHex.slice(2);
+		}
+		if (message.startsWith('0x')) {
+			message = message.slice(2);
+		}
+
+		// TODO: Do we want to simulate token expiration?
+		// let silentShareStorage = await getSilentShareStorage();
+		// let pairingData = silentShareStorage.pairingData;
+		// if (pairingData.tokenExpiration < Date.now() - TOKEN_LIFE_TIME) {
+		// 	pairingData = await refreshPairing();
+		// }
+		let messageHash = fromHexStringToBytes(messageHashHex);
+		if (messageHash.length !== 32) {
+			throw new SnapError(
+				'Invalid length of messageHash, should be 32 bytes',
+				SnapErrorCode.InvalidMessageHashLength,
+			);
+		}
+
+		return await SignAction.sign(
+			pairingData,
+			keyShare,
+			hashAlg,
+			message,
+			messageHash,
+			signMetadata,
+			accountId,
+		);
+	};
+
+export const mockEip1559Tx: any = {
+	type: '0x2',
+	nonce: '0x1',
+	to: TO_ADDRESS,
+	from: WALLER_ADDRESS,
+	value: '0x0',
+	data: '0x',
+	gasLimit: '0x5208',
+	maxPriorityFeePerGas: '0x3b9aca00',
+	maxFeePerGas: '0x2540be400',
+	accessList: [],
+	chainId: '0xaa36a7',
+};
+
+export const mockLegacyTx: any = {
+	type: '0x0',
+	nonce: '0x0',
+	to: TO_ADDRESS,
+	from: WALLER_ADDRESS,
+	value: '0x0',
+	data: '0x',
+	gasLimit: '0x5208',
+	gasPrice: '0x2540be400',
+	chainId: '0xaa36a7',
+};
+
+const exampleMessage = 'Example `personal_sign` message.';
+export const mockPersonalMsg = `0x${Buffer.from(
+	exampleMessage,
+	'utf8',
+).toString('hex')}`;
+export const mockSignTypedDataV4 = {
+	domain: {
+		chainId: '11155111',
+		name: 'Ether Mail',
+		verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
+		version: '1',
+	},
+	message: {
+		contents: 'Hello, Bob!',
+		from: {
+			name: 'Cow',
+			wallets: [
+				'0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
+				'0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF',
+			],
+		},
+		to: [
+			{
+				name: 'Bob',
+				wallets: [
+					'0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
+					'0xB0BdaBea57B0BDABeA57b0bdABEA57b0BDabEa57',
+					'0xB0B0b0b0b0b0B000000000000000000000000000',
+				],
+			},
+		],
+		attachment: '0x',
+	},
+	primaryType: 'Mail',
+	types: {
+		EIP712Domain: [
+			{
+				name: 'name',
+				type: 'string',
+			},
+			{
+				name: 'version',
+				type: 'string',
+			},
+			{
+				name: 'chainId',
+				type: 'uint256',
+			},
+			{
+				name: 'verifyingContract',
+				type: 'address',
+			},
+		],
+		Group: [
+			{
+				name: 'name',
+				type: 'string',
+			},
+			{
+				name: 'members',
+				type: 'Person[]',
+			},
+		],
+		Mail: [
+			{
+				name: 'from',
+				type: 'Person',
+			},
+			{
+				name: 'to',
+				type: 'Person[]',
+			},
+			{
+				name: 'contents',
+				type: 'string',
+			},
+			{
+				name: 'attachment',
+				type: 'bytes',
+			},
+		],
+		Person: [
+			{
+				name: 'name',
+				type: 'string',
+			},
+			{
+				name: 'wallets',
+				type: 'address[]',
+			},
+		],
+	},
+};
+
+export const mockSignTypedDataV3 = {
+    "types": {
+        "EIP712Domain": [
+            {
+                "name": "name",
+                "type": "string"
+            },
+            {
+                "name": "version",
+                "type": "string"
+            },
+            {
+                "name": "chainId",
+                "type": "uint256"
+            },
+            {
+                "name": "verifyingContract",
+                "type": "address"
+            }
+        ],
+        "Person": [
+            {
+                "name": "name",
+                "type": "string"
+            },
+            {
+                "name": "wallet",
+                "type": "address"
+            }
+        ],
+        "Mail": [
+            {
+                "name": "from",
+                "type": "Person"
+            },
+            {
+                "name": "to",
+                "type": "Person"
+            },
+            {
+                "name": "contents",
+                "type": "string"
+            }
+        ]
+    },
+    "primaryType": "Mail",
+    "domain": {
+        "name": "Ether Mail",
+        "version": "1",
+        "chainId": 11155111,
+        "verifyingContract": "0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC"
+    },
+    "message": {
+        "from": {
+            "name": "Cow",
+            "wallet": "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826"
+        },
+        "to": {
+            "name": "Bob",
+            "wallet": "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB"
+        },
+        "contents": "Hello, Bob!"
+    }
+}
+
+export const mockSignTypedDataV1 = [
+    {
+        "type": "string",
+        "name": "Message",
+        "value": "Hi, Alice!"
+    },
+    {
+        "type": "uint32",
+        "name": "A number",
+        "value": "1337"
+    }
+]
