c3600210e4a859bf3d3dd0de35a110e1fc75a13c
noahsilencelab <noah@silencelaboratories.com>
2024-06-10 12:09:28 +0700

    Add personal sign test case

--- src/__test__/index.test.ts
+++ src/__test__/index.test.ts
@@ -298,7 +298,7 @@ describe('test rpc requests to Snap', () => {
 				common,
 			});
 			expect(eip1559tx.verifySignature()).toEqual(true);
-			simulator.cleanUpSimulation();
+			await simulator.cleanUpSimulation();
 		});
 
 		it('tss_initPairing, tss_runPairing, tss_runKeygen, legacy tx signing should be success', async () => {
@@ -452,133 +452,138 @@ describe('test rpc requests to Snap', () => {
 				common: commonLegacy,
 			});
 			expect(legacyTx.verifySignature()).toEqual(true);
-			simulator.cleanUpSimulation();
+			await simulator.cleanUpSimulation();
 		});
 
-		// it('tss_initPairing, tss_runPairing, tss_runKeygen, personal signing should be success', async () => {
-		// 	const { request } = await installSnap();
-		// 	// Test init pairing
-		// 	const initPairingReq = request({
-		// 		method: InternalMethod.TssInitPairing,
-		// 		origin: ORIGIN,
-		// 		params: [{ isRePair: false }],
-		// 	});
+		it('tss_initPairing, tss_runPairing, tss_runKeygen, personal signing should be success', async () => {
+			const { request } = await installSnap();
+			// Test init pairing
+			const initPairingReq = request({
+				method: InternalMethod.TssInitPairing,
+				origin: ORIGIN,
+				params: [{ isRePair: false }],
+			});
 
-		// 	const ui = await initPairingReq.getInterface();
-		// 	expect(ui.type).toBe(DialogType.Confirmation);
-		// 	const prompt = INIT_PAIR_PANEL_HEADING;
-		// 	const description = INIT_PAIR_PANEL_DESCRIPTION;
-		// 	expect(ui).toRender(
-		// 		panel([
-		// 			heading(prompt),
-		// 			divider(),
-		// 			...description.map((t) => text(t)),
-		// 		]),
-		// 	);
+			const ui = await initPairingReq.getInterface();
+			expect(ui.type).toBe(DialogType.Confirmation);
+			const prompt = INIT_PAIR_PANEL_HEADING;
+			const description = INIT_PAIR_PANEL_DESCRIPTION;
+			expect(ui).toRender(
+				panel([
+					heading(prompt),
+					divider(),
+					...description.map((t) => text(t)),
+				]),
+			);
 
-		// 	await ui.ok();
+			await ui.ok();
 
-		// 	const initPairingJson: any = (await initPairingReq).response;
-		// 	const initPairingResult =
-		// 		initPairingJson.result as InitPairingResponse;
-		// 	const qrCode = initPairingResult.qrCode;
-		// 	const qrCodeObj = JSON.parse(qrCode) as QrCode;
+			const initPairingJson: any = (await initPairingReq).response;
+			const initPairingResult =
+				initPairingJson.result as InitPairingResponse;
+			const qrCode = initPairingResult.qrCode;
+			const qrCodeObj = JSON.parse(qrCode) as QrCode;
 
-		// 	expect(qrCodeObj.pairingId).toEqual(expect.any(String));
-		// 	expect(qrCodeObj.webEncPublicKey).toEqual(expect.any(String));
-		// 	expect(qrCodeObj.signPublicKey).toEqual(expect.any(String));
+			expect(qrCodeObj.pairingId).toEqual(expect.any(String));
+			expect(qrCodeObj.webEncPublicKey).toEqual(expect.any(String));
+			expect(qrCodeObj.signPublicKey).toEqual(expect.any(String));
 
-		// 	// Test run pairing
-		// 	await simulator.signIn();
-		// 	await simulator.pairing(qrCode);
+			// Test run pairing
+			await simulator.signIn();
+			await simulator.pairing(qrCode);
 
-		// 	const runPairingReq = request({
-		// 		method: InternalMethod.TssRunPairing,
-		// 		origin: ORIGIN,
-		// 	});
+			const runPairingReq = request({
+				method: InternalMethod.TssRunPairing,
+				origin: ORIGIN,
+			});
 
-		// 	const runPairingJson: any = (await runPairingReq).response;
-		// 	const runPairingResult =
-		// 		runPairingJson.result as RunPairingResponse;
-		// 	expect(runPairingResult.deviceName).toEqual(DEVICE_NAME);
-		// 	expect(runPairingResult.address).toBeNull();
+			const runPairingJson: any = (await runPairingReq).response;
+			const runPairingResult =
+				runPairingJson.result as RunPairingResponse;
+			expect(runPairingResult.deviceName).toEqual(DEVICE_NAME);
+			expect(runPairingResult.address).toBeNull();
 
-		// 	// Test key generation
-		// 	const keygenReq = request({
-		// 		method: InternalMethod.TssRunKeygen,
-		// 		origin: ORIGIN,
-		// 	});
-		// 	await simulator.keygen();
+			// Test key generation
+			const keygenReq = request({
+				method: InternalMethod.TssRunKeygen,
+				origin: ORIGIN,
+			});
+			await simulator.keygen();
 
-		// 	const keyGenJson: any = (await keygenReq).response;
-		// 	const runKeyGenResult = keyGenJson.result as RunKeygenResponse;
-		// 	expect(runKeyGenResult.address).toEqual(expect.any(String));
+			const keyGenJson: any = (await keygenReq).response;
+			const runKeyGenResult = keyGenJson.result as RunKeygenResponse;
+			expect(runKeyGenResult.address).toEqual(expect.any(String));
 
-		// 	request({
-		// 		method: InternalMethod.TssRunBackup,
-		// 		origin: ORIGIN,
-		// 	});
+			request({
+				method: InternalMethod.TssRunBackup,
+				origin: ORIGIN,
+			});
 
-		// 	// Test signing
-		// 	const keyshareReq = request({
-		// 		method: InternalMethod.E2eTestGetKeyShare,
-		// 		origin: ORIGIN,
-		// 	});
-		// 	const keyshareReqJson: any = (await keyshareReq).response;
-		// 	const keyshareResult = keyshareReqJson.result as {
-		// 		distributedKey: DistributedKey;
-		// 		pairingData: PairingData;
-		// 	};
+			// Test signing
+			const keyshareReq = request({
+				method: InternalMethod.E2eTestGetKeyShare,
+				origin: ORIGIN,
+			});
+			const keyshareReqJson: any = (await keyshareReq).response;
+			const keyshareResult = keyshareReqJson.result as {
+				distributedKey: DistributedKey;
+				pairingData: PairingData;
+			};
 
-		// 	const accountId = keyshareResult.distributedKey.accountId;
-		// 	const keyring = new SimpleKeyring({
-		// 		wallets: {
-		// 			[accountId]: {
-		// 				account: {
-		// 					id: 'f4211653-1b5f-4497-b5e6-f7b56824ba21',
-		// 					options: {},
-		// 					address: WALLER_ADDRESS,
-		// 					methods: [
-		// 						'eth_sign',
-		// 						'eth_signTransaction',
-		// 						'eth_signTypedData_v1',
-		// 						'eth_signTypedData_v3',
-		// 						'eth_signTypedData_v4',
-		// 						'personal_sign',
-		// 					],
-		// 					type: 'eip155:eoa',
-		// 				},
-		// 				distributedKey: keyshareResult.distributedKey,
-		// 			},
-		// 		},
-		// 		requests: {},
-		// 	});
+			const accountId = keyshareResult.distributedKey.accountId;
+			const keyring = new SimpleKeyring({
+				wallets: {
+					[accountId]: {
+						account: {
+							id: 'f4211653-1b5f-4497-b5e6-f7b56824ba21',
+							options: {},
+							address: WALLER_ADDRESS,
+							methods: [
+								'eth_sign',
+								'eth_signTransaction',
+								'eth_signTypedData_v1',
+								'eth_signTypedData_v3',
+								'eth_signTypedData_v4',
+								'personal_sign',
+							],
+							type: 'eip155:eoa',
+						},
+						distributedKey: keyshareResult.distributedKey,
+					},
+				},
+				requests: {},
+			});
 
-		// 	const pairingData = keyshareResult.pairingData;
-		// 	const runTssSign = genMockRunTssSign(pairingData);
+			const pairingData = keyshareResult.pairingData;
+			const runTssSign = genMockRunTssSign(pairingData);
 
-		// 	// Personal sign
-		// 	let personalSignResult: any = null;
-		// 	const exampleMessage = 'Example `personal_sign` message.';
-		// 	const msg = `0x${Buffer.from(exampleMessage, "utf8").toString("hex")}`;
-		// 	// "0x7b3456670d981c2216f2972be8d7a04c7aa94476"
-		// 	// '0x660265edc169bab511a40c0e049cc1e33774443d'
-		// 	keyring
-		// 		.signPersonalMessage(WALLER_ADDRESS, "0x4578616d706c652060706572736f6e616c5f7369676e60206d657373616765", runTssSign)
-		// 		.then((resp: any) => {
-		// 			personalSignResult = resp;
-		// 		})
-		// 		.catch((err) => {
-		// 			console.log('err', err);
-		// 		});
-		// 	await simulator.sign();
-		// 	while (!personalSignResult) {
-		// 		await delay(0);
-		// 		console.log('waiting for personalSignResult');
-		// 	}
+			// Personal sign
+			let personalSignResult: any = null;
+			const exampleMessage = 'Example `personal_sign` message.';
+			const msg = `0x${Buffer.from(exampleMessage, 'utf8').toString(
+				'hex',
+			)}`;
+			console.log('msg', msg);
+			keyring
+				.signPersonalMessage(
+					WALLER_ADDRESS,
+					'0x4578616d706c652060706572736f6e616c5f7369676e60206d657373616765',
+					runTssSign,
+				)
+				.then((resp: any) => {
+					personalSignResult = resp;
+				})
+				.catch((err) => {
+					console.log('err', err);
+				});
+			await simulator.sign();
+			while (!personalSignResult) {
+				await delay(0);
+				console.log('waiting for personalSignResult');
+			}
 
-		// 	simulator.cleanUpSimulation();
-		// });
+			await simulator.cleanUpSimulation();
+		});
 	});
 
 	// TODO: We only able to test this if Snap e2e package supports Keyring API
