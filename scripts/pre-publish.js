2961b54477b137da40d5f645c9b75c37e34021b7
Daksh Garg <daksh@silencelaboratories.com>
2024-07-02 14:53:33 +0530

    Update npm packages and add initial connection with pre-publish script

--- scripts/pre-publish.js
+++ scripts/pre-publish.js
@@ -14,6 +14,7 @@ let baseRepoUrl = '';
 let name = '';
 let rpcOrigins = [];
 let keyringOrigins = [];
+let initialConnectionOrigins = [];
 
 // Check the environment
 if (execEnv === 'staging') {
@@ -22,18 +23,20 @@ if (execEnv === 'staging') {
 		'https://www.npmjs.com/package/@silencelaboratories/silent-shard-snap-staging';
 	name = '@silencelaboratories/silent-shard-snap-staging';
     rpcOrigins = ["https://snap-staging.silencelaboratories.com", "http://localhost:3000"];
-    keyringOrigins = ["https://snap-staging.silencelaboratories.com", "http://localhost:3000"];
 } else if (execEnv === 'production') {
 	console.log(`${chalk.green.bold('PREPUBLISH PRODUCTION ANNOUNCEMENT')}\u{1F447}\u{1F447}\u{1F447}`);
 	baseRepoUrl =
 		'https://www.npmjs.com/package/@silencelaboratories/silent-shard-snap';
 	name = '@silencelaboratories/silent-shard-snap';
     rpcOrigins = ["https://snap.silencelaboratories.com"];
-    keyringOrigins = ["https://snap.silencelaboratories.com"];
 } else {
 	console.log(chalk.red(`Invalid environment: ${execEnv}`));
 	throw new Error('Invalid environment');
 }
+
+keyringOrigins = rpcOrigins;
+initialConnectionOrigins = rpcOrigins;
+
 const announcement = chalk.italic(
 	`We will publish new version of ${chalk.yellow(
 		name,
@@ -53,6 +56,12 @@ packageJson.homepage = baseRepoUrl;
 // Override snap.manifest.json fields
 snapManifestJson.initialPermissions['endowment:rpc'].allowedOrigins = rpcOrigins;
 snapManifestJson.initialPermissions['endowment:keyring'].allowedOrigins = keyringOrigins;
+snapManifestJson.initialConnections = initialConnectionOrigins.reduce((acc, key) => {
+  acc[key] = {};
+  return acc;
+}, {});;
+
+
 
 // Write the package.json file
 fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2), 'utf8');
